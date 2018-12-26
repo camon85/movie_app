0e47805d178ae3e9af16f93cb2e173ef798f5576
Jooyong Sung <camon85@gmail.com>
2018-12-27 01:07:08 +0900

    data flow with props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
+const movieTitles = [
+  "Matrix",
+  "Hunger Game",
+  "Oldboy",
+  "Star Wars"
+]
+
+const movieImages = [
+  "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+  "https://imgix.bustle.com/uploads/image/2017/8/9/5846855f-3fd5-4db9-84cb-5f65ed2bd107-hunger-games.jpg?w=375&h=211&fit=crop&crop=faces&auto=format&q=70&dpr=2",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+  "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
+        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
+        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
+        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
+        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -5,8 +5,8 @@ class Movie extends Component {
   render() {
     return (
         <div>
-            <MoviePoster/>
-            <h1>Hello this is a movie</h1>
+            <MoviePoster poster={this.props.poster}/>
+            <h1>{this.props.title}</h1>
         </div>
     )
   }
@@ -15,7 +15,7 @@ class Movie extends Component {
 class MoviePoster extends Component {
     render() {
         return (
-            <img src="https://imgix.bustle.com/uploads/image/2017/8/9/5846855f-3fd5-4db9-84cb-5f65ed2bd107-hunger-games.jpg?w=375&h=211&fit=crop&crop=faces&auto=format&q=70&dpr=2"/>
+            <img src={this.props.poster}/>
         )
     }
 }

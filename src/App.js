a0a4adde36d147011dd353b3857513b93cb71f9d
Jooyong Sung <camon85@gmail.com>
2018-12-28 22:10:50 +0900

    practicing setState

--- src/App.js
+++ src/App.js
@@ -2,25 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
-  },
-  {
-    title: "Hunger Game",
-    poster: "https://imgix.bustle.com/uploads/image/2017/8/9/5846855f-3fd5-4db9-84cb-5f65ed2bd107-hunger-games.jpg?w=375&h=211&fit=crop&crop=faces&auto=format&q=70&dpr=2"
-  },
-  {
-    title: "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
-  },
-]
-
 class App extends Component {
 
   /*
@@ -40,7 +21,24 @@ class App extends Component {
 
 
   state = {
-    greeting: 'hello!'
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
+      },
+      {
+        title: "Hunger Game",
+        poster: "https://imgix.bustle.com/uploads/image/2017/8/9/5846855f-3fd5-4db9-84cb-5f65ed2bd107-hunger-games.jpg?w=375&h=211&fit=crop&crop=faces&auto=format&q=70&dpr=2"
+      },
+      {
+        title: "Oldboy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+      },
+      {
+        title: "Star Wars",
+        poster: "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
+      },
+    ]
   }
 
   render() {
@@ -48,8 +46,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
@@ -58,6 +55,17 @@ class App extends Component {
 
   componentDidMount() {
     console.log('= componentDidMount =');
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          ...this.state.movies,
+          {
+            title: '아쿠아맨',
+            poster: 'http://img.movist.com/?img=/x00/05/10/29_p1.jpg'
+          }
+        ]
+      });
+    }, 1000);
   }
 
 }

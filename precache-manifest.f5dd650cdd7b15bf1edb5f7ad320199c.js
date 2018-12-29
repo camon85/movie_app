f180a1aa1f91d82e818e69ff310eaf0f8950614a
Jooyong Sung <camon85@gmail.com>
2018-12-29 17:25:30 +0900

    Updates

---
+++ precache-manifest.f5dd650cdd7b15bf1edb5f7ad320199c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23b573c6de720355cd37",
+    "url": "/movie_app/static/css/main.b064ac0f.chunk.css"
+  },
+  {
+    "revision": "23b573c6de720355cd37",
+    "url": "/movie_app/static/js/main.23b573c6.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "df3163b6db3ce11da97cf2d8c91d46c6",
+    "url": "/movie_app/index.html"
+  }
+];
